fbdd8e123be134eff9779a0cddac90c20f1daca7
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2024-07-20 12:41:06 -0300

    chore: to avoid all routes being dynamic, we want home page to be static. in order to do so, we have to change the Header component since it may be operating cookies (which automatically makes the page dynamic). In order to change this, we have to make a HeaderAuth CLIENT component instead of SERVER component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,59 +1,15 @@
-import React from "react";
-import Link from "next/link";
+import React from "react"
+import Link from "next/link"
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@nextui-org/react";
-import { auth } from "@/auth";
-import * as actions from "@/actions";
+} from "@nextui-org/react"
+import HeaderAuth from "./HeaderAuth"
 
-async function Header() {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ""} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
+function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -68,13 +24,11 @@ async function Header() {
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">{authContent}</NavbarContent>
-
-      <NavbarContent></NavbarContent>
-
-      <NavbarContent></NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
-  );
+  )
 }
 
-export default Header;
+export default Header

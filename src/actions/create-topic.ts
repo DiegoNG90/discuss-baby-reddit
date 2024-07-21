631b921e1ad15299cba6820ad08ccf40391cdf79
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2024-07-20 22:56:55 -0300

    feat: update create-topic action to save the recorded Topic on PrismaDB and capture a _form error if user not logged in as well. Also, add revalidatePath cache command so it redirects with updated data

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -2,6 +2,11 @@
 
 import { z } from "zod"
 import { auth } from "@/auth"
+import type { Topic } from "@prisma/client"
+import { redirect } from "next/navigation"
+import { db } from "@/db"
+import { paths } from "@/paths"
+import { revalidatePath } from "next/cache"
 
 const createTopicSchema = z.object({
   name: z
@@ -27,7 +32,6 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
-  // Revalidate the HOMEPage after creating a topic
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
@@ -49,7 +53,30 @@ export async function createTopic(
     }
   }
 
-  return {
-    errors: {},
+  let topic: Topic
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    })
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      }
+    } else {
+      return {
+        errors: {
+          _form: ["Unknown error: something went wrong"],
+        },
+      }
+    }
   }
+
+  revalidatePath("/")
+  redirect(paths.topicShow(topic.slug))
 }

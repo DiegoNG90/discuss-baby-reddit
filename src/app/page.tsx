44883c79cc551d2409420a6082c475270271a42b
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2024-07-25 23:17:55 -0300

    chore: pass PostList component on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { fetchTopPosts } from "@/db/queries/posts"
+import { PostList } from "./components/Post/post-list"
 import TopicCreateForm from "./components/Topics/TopicCreateForm"
 import TopicList from "./components/Topics/TopicList"
 import { Divider } from "@nextui-org/react"
+import { db } from "@/db"
 
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top post</h1>
+        <PostList fetchData={() => fetchTopPosts()} />
       </div>
       <div className="border shadow py-2 px-2">
         <TopicCreateForm />

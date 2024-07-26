4c199bbcf47d69a47d6fea31f7afd7a0d6e93be3
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2024-07-25 22:57:36 -0300

    chore: use nextUi Skeleton component and tailwind to build PostShowLoading component and render it as Suspense fallback on PostShowPage

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -5,6 +5,7 @@ import CommentList from "@/app/components/comments/comment-list"
 import CommentCreateForm from "@/app/components/comments/comment-create-form"
 import paths from "@/paths"
 import { parseSlug } from "@/app/utils/slug"
+import PostShowLoading from "@/app/components/Post/post-show-loading"
 
 interface PostShowPageProps {
   params: {
@@ -22,7 +23,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {parsedSlug}
       </Link>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<PostShowLoading />}>
         <PostShow postId={postId} />
       </Suspense>
       <CommentCreateForm postId={postId} startOpen />
